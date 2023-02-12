7f4ceb33a8164ccd70c3f791ec43c66f8a48a784
RUNAaa0712 <88968084+RUNAaa0712@users.noreply.github.com>
2023-02-12 23:39:26 +0900

    Add files via upload

---
+++ @FdfXe/script.js
@@ -0,0 +1,40 @@
+document.getElementById("flr-one").addEventListener("click",()=>{
+    let all_ele = document.querySelectorAll(".tab_content");
+    all_ele.forEach((value,num)=>{ (num==0) ? all_ele[num].style.display = "block" : all_ele[num].style.display = "none"; })
+})
+document.getElementById("flr-two").addEventListener("click",()=>{
+    let all_ele = document.querySelectorAll(".tab_content");
+    all_ele.forEach((value,num)=>{ (num==1) ? all_ele[num].style.display = "block" : all_ele[num].style.display = "none"; })
+})
+document.getElementById("flr-three").addEventListener("click",()=>{
+    let all_ele = document.querySelectorAll(".tab_content");
+    all_ele.forEach((value,num)=>{ (num==2) ? all_ele[num].style.display = "block" : all_ele[num].style.display = "none"; })
+})
+document.getElementById("flr-four").addEventListener("click",()=>{
+    let all_ele = document.querySelectorAll(".tab_content");
+    all_ele.forEach((value,num)=>{ (num==3) ? all_ele[num].style.display = "block" : all_ele[num].style.display = "none"; })
+})
+document.getElementById("flr-five").addEventListener("click",()=>{
+    let all_ele = document.querySelectorAll(".tab_content");
+    all_ele.forEach((value,num)=>{ (num==4) ? all_ele[num].style.display = "block" : all_ele[num].style.display = "none"; })
+})
+document.getElementById("flr-six").addEventListener("click",()=>{
+    let all_ele = document.querySelectorAll(".tab_content");
+    all_ele.forEach((value,num)=>{ (num==5) ? all_ele[num].style.display = "block" : all_ele[num].style.display = "none"; })
+})
+document.getElementById("flr-seven").addEventListener("click",()=>{
+    let all_ele = document.querySelectorAll(".tab_content");
+    all_ele.forEach((value,num)=>{ (num==6) ? all_ele[num].style.display = "block" : all_ele[num].style.display = "none"; })
+})
+document.getElementById("flr-eight").addEventListener("click",()=>{
+    let all_ele = document.querySelectorAll(".tab_content");
+    all_ele.forEach((value,num)=>{ (num==7) ? all_ele[num].style.display = "block" : all_ele[num].style.display = "none"; })
+})
+document.getElementById("flr-nine").addEventListener("click",()=>{
+    let all_ele = document.querySelectorAll(".tab_content");
+    all_ele.forEach((value,num)=>{ (num==8) ? all_ele[num].style.display = "block" : all_ele[num].style.display = "none"; })
+})
+document.getElementById("flr-ten").addEventListener("click",()=>{
+    let all_ele = document.querySelectorAll(".tab_content");
+    all_ele.forEach((value,num)=>{ (num==9) ? all_ele[num].style.display = "block" : all_ele[num].style.display = "none"; })
+})
